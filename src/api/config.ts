3daa522ab705bc989f891a5b87e260cd150e062b
Lucio Ruben Villena <luciovillena7@gmail.com>
2026-03-24 20:57:56 -0400

    change servos prototype and config.ts

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,7 +2,7 @@ export const DEFAULT_ESP32_HOST = "192.168.4.1"; // ESP32 AP default gateway IP
 export const DEFAULT_PROTOCOL: "http" | "https" = "http";
 
 export const REST_PORT = 80; // usually not needed explicitly
-export const WS_PORT = 80;
+export const WS_PORT = 81;
 
 export function makeBaseUrl(
   host: string,
@@ -14,5 +14,5 @@ export function makeBaseUrl(
 export function makeWsUrl(host: string, path = "/ws/manual") {
   // If you ever use https, WS becomes wss:
   const scheme = DEFAULT_PROTOCOL === "https" ? "wss" : "ws";
-  return `${scheme}://${host}${path}`;
+  return `${scheme}://${host}:${WS_PORT}${path}`;
 }
